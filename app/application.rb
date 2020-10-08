e7346bb78420604567d55b163ad11434e8c76942
Roland McRae <mcraeroland@gmail.com>
2020-10-08 18:52:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,11 @@
 class Application
 
   def call(env)
+    resp = Rack::Response.new
 
 
 
-
-    if Time.now.to_i > 12
+    if Time.now.to_i >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
